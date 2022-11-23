9597ad787507a42340611ba433699160cd970d33
Jorge Santos <jdsantos@users.noreply.github.com>
2022-11-23 13:55:32 +0000

    Added webpack to wrap things up

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/maplibre-gl-measures.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'maplibre-gl-measures.js',
+        library: "maplibreGLMeasures",
+    },
+};
