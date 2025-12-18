cfb2337b8c22549eeb07985568dd8ecadc17aaf8
Jorge Santos <jorgedmsantos@gmail.com>
2025-12-18 22:16:25 +0000

    chore: improved ts support

---
+++ dist/maplibre-gl-measures.d.ts
@@ -0,0 +1,51 @@
+declare module 'maplibre-gl-measures' {
+	import { Map } from 'maplibre-gl';
+
+	interface MeasuresControlOptions {
+		lang?: {
+			areaMeasurementButtonTitle?: string;
+			lengthMeasurementButtonTitle?: string;
+			clearMeasurementsButtonTitle?: string;
+		};
+		units?: 'imperial' | 'metric';
+		unitsGroupingSeparator?: string;
+		style?: {
+			text?: {
+				radialOffset?: number;
+				letterSpacing?: number;
+				color?: string;
+				haloColor?: string;
+				haloWidth?: number;
+				font?: string;
+			};
+			common?: {
+				midPointRadius?: number;
+				midPointColor?: string;
+				midPointHaloRadius?: number;
+				midPointHaloColor?: string;
+			};
+			areaMeasurement?: {
+				fillColor?: string;
+				fillOutlineColor?: string;
+				fillOpacity?: number;
+				lineWidth?: number;
+			};
+			lengthMeasurement?: {
+				lineWidth?: number;
+				lineColor?: string;
+			};
+		};
+		onRender?: (features: any) => void;
+		onCreate?: (features: any) => void;
+	}
+
+	class MeasuresControl {
+		constructor(options: MeasuresControlOptions);
+
+		onAdd(map: Map): HTMLElement;
+
+		onRemove(): void;
+	}
+
+	export default MeasuresControl;
+}
